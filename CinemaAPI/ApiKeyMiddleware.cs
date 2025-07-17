2b01d78a86cc3ae5688689abc14325a1c025ac3c
Henneth Malatji <malatji.hen@gmail.com>
2025-07-18 01:02:32 +0200

    Added API Key Authentication to the API

---
+++ CinemaAPI/ApiKeyMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace CinemaAPI
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public const string APIKEY_HEADER = "X-Api-Key";
+
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IConfiguration configuration) { 
+        
+        
+        if(!context.Request.Headers.TryGetValue(APIKEY_HEADER, out var extractedApiKey)) {
+
+                context.Response.StatusCode = 401;// Unathorized
+                await context.Response.WriteAsync("API Key is missing");
+                return;
+        }
+        
+        var apiKey = configuration.GetValue<string>("ApiKey");
+            if (!apiKey.Equals(extractedApiKey)) {
+
+                context.Response.StatusCode = 401;// Unathorized
+                await context.Response.WriteAsync("Invalid Api Key");
+                return;
+            }
+        
+            await _next(context);
+        }
+    }
+}
--- CinemaAPI/Program.cs
+++ CinemaAPI/Program.cs
@@ -1,5 +1,7 @@
+using CinemaAPI;
 using CinemaAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,40 @@ options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+//Add Api key Support in Swagger UI ..Modify Swagger Configuration In Program.cs 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "My API", Version = "v1" });
+
+    // Add API Key support
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "API Key needed to access the endpoints. Use: X-Api-Key: {your_api_key}",
+        In = ParameterLocation.Header,
+        Name = "X-Api-Key",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "ApiKeyScheme"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                },
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
 
 var app = builder.Build();
 
@@ -26,6 +61,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.MapControllers();
 
